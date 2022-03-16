b02e2bb9235e6751cbca4715efc1fbeac2fa0b93
samueltotoro <Arrudarosasamuel@gmai.com>
2022-03-16 09:32:58 -0300

    Projeto HotSite

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hotsite.Models;
+using MySql.Data.MySqlClient;
+
+namespace Hotsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Interesse cad)
+        {
+            try
+            {
+            DatabaseService dbs = new DatabaseService();
+            dbs.CadastraInteresse(cad);
+            return Json(new{StatusCode="Ok",mensagem="Gravado com sucesso!"});
+            }catch(Exception e)
+            {
+                _logger.LogError("Erro: " + e.Message);
+                return Json(new {status="ERRO",mensagem="Falha ao gravar os dados, realize a ação mais tarde!"});
+            }
+
+        }
+
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,52 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function Cadastrar()
+{
+    let parametros = {
+        Nome: $("#nome").val(),
+        Email: $("#email").val(),
+        Mensagem: $("#mensagem").val()
+    }
+    
+    $.post('/Home/Cadastrar',parametros)
+
+    .done(
+        function(data)
+        {
+            if (data.status=="OK") 
+            {
+                $("#frm").after("<h3>Dado cadastrado com sucesso!</h3>");
+                $("#frm").hide();
+            } 
+            else
+            {
+                $("#frm").after("<h3>"+data.mensagem+"</h3>");
+            }
+        }
+    ) 
+    
+    .fail(
+    function()
+    {
+        alert("Ocorreu algo errado!");
+    }
+    );
+
+}
+
+$(document).ready(
+    function()
+    {
+        $("#frmCadastro").submit
+        (
+            function(e)
+            {
+                e.preventDefault();
+                Cadastrar();
+            }
+        );
+    }
+);
